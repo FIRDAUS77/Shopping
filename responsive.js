98c5a40176fb89f85be3dfb3fc21e1572ce87978
Firdaus Ahmed Siddiqui <64842162+FIRDAUS77@users.noreply.github.com>
2020-12-02 23:03:24 +0530

    Delete responsive.js

--- responsive.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.burger').addEventListener("click",function() {
-    document.querySelector(".nav-list-right").classList.remove('v-class-resp');
-    document.querySelector(".nav-list-left").classList.remove('v-class-resp');
-    document.querySelector(".navbar").classList.toggle('height-class-resp');
-
-})
